2aa0c81fa1ca3a051a62ee7e4a35e011fab25da7
Ankita Gharti Magar <ankitagmgr@gmail.com>
2025-04-01 21:57:56 +0545

    displaying title

--- budget_tracker/views.py
+++ budget_tracker/views.py
@@ -3,7 +3,9 @@ from budget_tracker.models import Transaction
 from django.http import HttpResponseRedirect
 
 def transaction_list(request):
+    transactions=Transaction.objects.all()
     return render(
         request,
         'transaction.html',
+        {"transactions":transactions}
     )
