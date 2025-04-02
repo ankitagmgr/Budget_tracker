e77c68cc28c4ffe089b7f27f2b818c178f3c8d2b
Ankita Gharti Magar <alovotae@gmail.com>
2025-04-02 14:25:54 +0545

    more view

--- budget_tracker/urls.py
+++ budget_tracker/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('',views.transaction_list,name="transaction-list")
+    path('',views.transaction_list,name="transaction-list"),
+    path('add/',views.add_transaction, name='add-transaction')
 ]
--- budget_tracker/views.py
+++ budget_tracker/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from budget_tracker.models import Transaction
 from django.http import HttpResponseRedirect
 
@@ -9,3 +9,20 @@ def transaction_list(request):
         'transaction.html',
         {"transactions":transactions}
     )
+def add_transaction(request):
+    if request.method == "POST":
+        title = request.POST.get("title")
+        amount = request.POST.get("amount")
+        type_ = request.POST.get("type")
+        category = request.POST.get("category")
+
+        if title and amount and type_ and category:  # Ensure required fields are provided
+            Transaction.objects.create(
+                title=title,
+                amount=amount,
+                type=type_,
+                category=category
+            )
+            return redirect("transaction_list")  # Redirect to transaction list after adding
+
+    return render(request, "add_transaction.html")
